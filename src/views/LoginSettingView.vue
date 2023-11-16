e5885de52b26d771de80a0c3f367d1fe98c3812e
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2023-11-16 21:19:05 +0900

    에러 코드 변경

--- src/views/LoginSettingView.vue
+++ src/views/LoginSettingView.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="login" >
-    <div class="logo-area">
-      이메일을 설정해주세요
-      <input type="text" name="email" class="border border-slate-300 w-full" :value="email"/>
+    <div class="logo-area pb-16">
+      <img :src="logo" lt="planLogo" />
+      <p>
+        PLANDLY<br />
+        시작하기
+      </p>
+    </div>
 
-      닉네임을 설정해주세요
-      <input type="text" name="email" class="border border-slate-300 w-full" :value="displayName"/>
-      
+    <div class="logo-area">
+      이메일을 설정해주세요<br/>
+      <input type="text" name="email" class="border border-slate-300 w-72 mb-5" v-model="email"/>
+      <br/>
+      닉네임을 설정해주세요<br/>
+      <input type="text" name="email" class="border border-slate-300 w-72 mb-5" v-model="displayName"/>
+      <br/>
       <button
         color="indigo"
-        class="items-start naver"
+        class="bg-green-100 p-2 text-sm w-72"
         @click="setEmail"
         >
           <span>기본정보 설정하기</span>
-      </button>);
+      </button>
     </div>
   </div>
 </template>
     
 <script setup lang='ts'>
-  import {ref} from 'vue'
+  import logo from "@/assets/img/logo.png";
+  import { ref } from "vue";
   import api from '@/axios/api'
   import { useUserStore } from "@/stores/user.js";
   import { useRouter } from "vue-router";
@@ -34,13 +43,21 @@
   const setEmail = () => {
     const param = {
       email: email.value,
-      uid: store.userInfo.uid,
-      displayName: displayName.value
+      displayName: displayName.value,
+      uid: store.userInfo.uid
     }
     api.setEmail(param)
     .then(({data}) => {
       console.log(data)
       if(data.code === 0) router.push('/')
+    })
+    .catch((e) => {
+      if(e.response.status === 400){
+        if(e.response.data.code === 2) alert("이미 사용되고 있는 email입니다.")
+        else alert("Server Error")
+      } else {
+        alert("Server Error")
+      }
     })
   }
 </script>
