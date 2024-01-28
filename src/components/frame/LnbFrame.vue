0fc4e109ff26a6f2e81938355769cd55ca2c82bf
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2024-01-28 20:58:30 +0900

    svg 로더 import

--- src/components/frame/LnbFrame.vue
+++ src/components/frame/LnbFrame.vue
@@ -12,15 +12,19 @@
 
     <ul>
       <li>메뉴</li>
-      <li><img :src="iconSetting" />계정 설정</li>
-      <li><img :src="iconFriends" />친구 리스트</li>
+      <li><iconSetting/>계정 설정</li>
+      <li><iconFriends />친구 리스트</li>
     </ul>
 
     <ul>
       <li>모임 관리</li>
-      <li><img :src="iconMeeting" />모임 설정</li>
-      <li><img :src="iconMember" />멤버 관리</li>
-      <li><img :src="iconShare" />모임 공유</li>
+      <li><iconMeeting />모임 설정</li>
+      <li><iconMember />멤버 관리</li>
+      <li><iconShare />모임 공유</li>
+    </ul>
+
+    <ul>
+      <li>내 모임리스트 <iconPlus/> </li>
     </ul>
   </nav>
 </template>
@@ -31,6 +35,7 @@ import iconSetting from '@/assets/img/lnb/icon_setting.svg'
 import iconMeeting from '@/assets/img/lnb/icon_meeting.svg'
 import iconMember from '@/assets/img/lnb/icon_member.svg'
 import iconShare from '@/assets/img/lnb/icon_share.svg'
+import iconPlus from '@/assets/img/common/icon_plus.svg';
 import { useUserStore } from '@/stores/user.js'
 const store = useUserStore()
 </script>
@@ -50,7 +55,7 @@ const store = useUserStore()
       padding: 12px 0px;
     }
     li {
-      img {
+      svg {
         margin-right: 20px;
       }
       padding: 18px 24px;
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), svgLoader()],
 	define: {
 		...(process.env.NODE_ENV === 'development' ? { global: 'window' } : {}),
 	},
@@ -13,5 +14,6 @@ export default defineConfig({
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
-	},
+	},	
+	
 });
