65c21c4b3450ffac58b2b82f30000de621c99e0c
DaYoung-woo <iamwooda0@gmail.com>
2024-01-29 17:57:15 +0900

    충돌 해결

--- src/components/common/PageLoading.vue
+++ src/components/common/PageLoading.vue
@@ -1,59 +1,58 @@
 <template>
-	<div :class="`${isBlack && 'modal-back'} h-full`" >
-		<div class="flex items-center h-full w-full justify-center flex-col">
-			<div class="droplet_spinner">
-				<div class="droplet"></div>
-				<div class="droplet"></div>
-				<div class="droplet"></div>
-			</div>
-			<img :src="LogoIcon" :style="`width:${width}`"/>
-		</div>
-	</div>
+  <div :class="`${isBlack && 'modal-back'} h-full`">
+    <div class="flex items-center h-full w-full justify-center flex-col">
+      <div class="droplet_spinner">
+        <div class="droplet"></div>
+        <div class="droplet"></div>
+        <div class="droplet"></div>
+      </div>
+      <img :src="LogoIcon" :style="`width:${width}`" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import LogoIcon from '@/assets/img/logo_icon.svg';
+import LogoIcon from '@/assets/img/logo_icon.svg'
 
 defineProps<{
-  isBlack?: Boolean,
-	width:String
+  isBlack?: Boolean
+  width: String
 }>()
-
 </script>
 
 <style>
 .modal-back {
-	background: rgba(0, 0, 0, 0.7);
+  height: 100vh;
 }
 .droplet_spinner {
-	display: flex;
-	justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 .droplet_spinner .droplet {
-	width: 12px;
-	height: 12px;
-	margin: 0 5px;
-	background-color: #fcc644;
-	border-radius: 50%;
-	transform-origin: center bottom;
-	animation: bounce 1.2s cubic-bezier(0.3, 0.01, 0.4, 1) infinite;
+  width: 12px;
+  height: 12px;
+  margin: 0 5px;
+  background-color: #fcc644;
+  border-radius: 50%;
+  transform-origin: center bottom;
+  animation: bounce 1.2s cubic-bezier(0.3, 0.01, 0.4, 1) infinite;
 }
 .droplet_spinner .droplet:nth-child(1) {
-	animation-delay: -0.4s;
+  animation-delay: -0.4s;
 }
 .droplet_spinner .droplet:nth-child(2) {
-	animation-delay: -0.2s;
+  animation-delay: -0.2s;
 }
 .droplet_spinner .droplet:nth-child(3) {
-	animation-delay: 0s;
+  animation-delay: 0s;
 }
 @keyframes bounce {
-	0%,
-	100% {
-		transform: translateY(-20px);
-	}
-	50% {
-		transform: translateY(-35px);
-	}
+  0%,
+  100% {
+    transform: translateY(-20px);
+  }
+  50% {
+    transform: translateY(-35px);
+  }
 }
 </style>
