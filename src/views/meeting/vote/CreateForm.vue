af03286c6aee511714d370b99fc70402e466e10f
DaYoung-woo <iamwooda0@gmail.com>
2024-02-27 23:43:15 +0900

    투표 상세 페이지 퍼블리싱 진행

--- src/views/meeting/vote/CreateForm.vue
+++ src/views/meeting/vote/CreateForm.vue
@@ -1,28 +1,38 @@
 <template>
   <!-- 페이지 네비게이션 -->
   <router-link :to="`/meeting/${meetingNo}`" class="path_navi mt-10">
-    <IconArrowLeft class="mr-1" />게시판
+    <IconArrowLeft class="mr-1" />투표
   </router-link>
 
   <!-- 작성자 정보 -->
-  <div class="flex justify-between items-center mt-8 mb-4">
-    <MemberProfile />
-    <IconMore fill="#cdcdcd" />
+  <div class="mt-8 mb-4">
+    <h2>새 투표 작성하기</h2>
   </div>
-  <hr />
 
   <!-- 게시판 폼 -->
-  <form action=""></form>
+  <form action="">
+    <input
+      type="text"
+      class="w-full outline-none p-2 mb-8"
+      placeholder="어떤 투표인지 설명해주세요"
+      style="border-bottom: 1px solid #ddd"
+    />
+
+    <label class="form-label mb-3">투표명</label>
+    <input type="text" class="form-inputs w-full" placeholder="투표명 작성" />
+
+    <label class="form-label mb-3 mt-12">선택지 입력</label>
+    <div class="flex">
+      <input type="checkbox" id="text" /> <label for="text">텍스트</label>
+      <input type="checkbox" id="date" /> <label for="date">날짜</label>
+      <input type="checkbox" id="location" /><label for="location">장소</label>
+    </div>
+  </form>
 </template>
 
 <script setup lang="ts">
-// 멤버프로필 컴포넌트
-import MemberProfile from '@/components/common/MemberProfile.vue'
-
 // < 아이콘
 import IconArrowLeft from '@/assets/img/common/icon_arrow_left.svg'
-// 더보기 아이콘
-import IconMore from '@/assets/img/common/icon_more.svg'
 
 // 라우터
 import { useRoute } from 'vue-router'
